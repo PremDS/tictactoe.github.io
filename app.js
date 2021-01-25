de58289e5fdff9963c3e332e738a1a2928a77e6a
Prem-K-Tamang <ktamangprem@gmail.com>
2021-01-26 00:30:00 +0545

    Tic Tac Toe Completed

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 // Restart Game Button
-
+let restart = document.getElementById("btn-restart");
 
 
 // Grabs all the squares
-
+let squares = document.querySelectorAll("td");
 
 //Clear all the squares
+function clearBoard() {
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].textContent="";
+        
+    }
+}
 
 
 //Check the square marker
+function changeMarker() {
+    if(this.textContent==='') {
+        this.textContent= 'X';
+    } else if (this.textContent==='X') {
+        this.textContent='O';
+    } else {
+        this.textContent='';
+    }
+}
 
 
+for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click',changeMarker);
+    
+}
 // For loop to add event listeners to all the squares
+restart.addEventListener("click", clearBoard);
